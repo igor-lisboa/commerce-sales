91a0ca2d75dd4e99d9f3f50fbc9749cb8b7133f6
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2020-11-23 23:21:52 -0300

    atualizando pagina de atualizacao d senha

--- resources/views/user/update-password.blade.php
+++ resources/views/user/update-password.blade.php
@@ -4,6 +4,8 @@
 <form method="POST" action="<?= route('update_password', [$user->remember_token]) ?>">
     <fieldset>
         @csrf
+        <h2>Olá {{$user->name}},</h2>
+        <small>Atualize sua senha!</small>
         <input placeholder="Nova Senha" autocomplete="current-password" type="password" name="password" required value="<?= old('password') ?>" />
         <input placeholder="Confirmação da Nova Senha" autocomplete="current-password" type="password" name="confirm_password" required value="<?= old('confirm_password') ?>" />
         <button type="submit">Atualizar senha</button>
