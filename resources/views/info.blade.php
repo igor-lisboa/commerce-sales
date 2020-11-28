ece44b37967715dbe07717b73a3c455021f7c4a1
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2020-11-28 12:51:00 -0300

    atualizando lista dos contribuidores do projeto

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,8 +1,27 @@
 <hr>
 <h1>O trabalho foi feito por:</h1>
 <ul>
-    <li>Caio Wey</li>
-    <li>Igor Lisboa</li>
+    <li>
+        <a href="https://github.com/caiowbarros" target="_blank">Caio Wey</a>
+    </li>
+    <li>
+        <a href="https://github.com/gabrielodd" target="_blank">Gabriel Sena</a>
+    </li>
+    <li>
+        <a href="https://github.com/igor-lisboa" target="_blank">Igor Lisboa</a>
+    </li>
+    <li>
+        <a href="https://github.com/LuizGPereira" target="_blank">Luiz Gustavo Pereira</a>
+    </li>
+    <li>
+        <a href="https://github.com/marizeraus" target="_blank">Mariana Suarez</a>
+    </li>
+    <li>
+        <a href="https://github.com/Rafa2266" target="_blank">Rafael Franca Scofield Lauar</a>
+    </li>
+    <li>
+        <a href="https://github.com/vitorcuzzuol" target="_blank">Vitor Azevedo</a>
+    </li>
 </ul>
 <h3>Usamos <a href="https://laravel.com">Laravel</a> para desenvolver.</h3>
 <a href="https://drive.google.com/drive/folders/1VbdfnAtDPqReQlx2Qnu7xCf6Et_zDrnx" target="_blank">Os diagramas podem ser encontrados aqui!</a>
